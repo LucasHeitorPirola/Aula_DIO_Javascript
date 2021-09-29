b5a30d7fd82b0aad48f0f5d645ab6b9a2a165fc3
LucasHeitorPirola <lucas_heitor_pirola@hotmail.com>
2021-09-29 00:58:34 -0300

    Adição do arquivo html e do arquivojs com alguns códigos escritos para treino

---
+++ main.js
@@ -0,0 +1,9 @@
+//Início do projeto que eu nem sei o que será
+var nome = "Lucas Pirola";
+var idade = 21
+var idade2 = 10
+var frase = "Japão é o melhor país do mundo"
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+console.log(nome + [idade + idade2])
+console.log(frase.replace("Japão", "Brasil"));
