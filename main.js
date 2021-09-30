e1cc677ea219ecc176a0d9c63d0745cba6badb98
LucasHeitorPirola <lucas_heitor_pirola@hotmail.com>
2021-09-29 23:38:27 -0300

    Segunda aula, dessa vez exploramos arrays e montamos uma espécie de dicionário

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 //Início do projeto que eu nem sei o que será
-var nome = "Lucas Pirola";
-var idade = 21
-var idade2 = 10
-var frase = "Japão é o melhor país do mundo"
+
+//Dicionário
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"laranja", cor:"laranja"}];
+console.log(frutas[0])
+*/
+
+//Arrays
+
+//var lista = ["maça", "pêra", "laranja"];
+//console.log(lista.toString()[5]);
+//console.log(lista.join("-"));
+//console.log(lista.reverse());
+//console.log(lista.length);
+//lista.pop();
+//lista.push("melancia");
+
+//Variáveis
+
+//var nome = "Lucas Pirola";
+//var idade = 21
+//var idade2 = 10
+//var frase = "Japão é o melhor país do mundo"
+
+//alert
+
 //alert(nome + " tem " + idade + " anos ");
 //alert(idade + idade2);
-console.log(nome + [idade + idade2])
-console.log(frase.replace("Japão", "Brasil"));
+
+//console.log 
+
+//console.log(nome + [idade + idade2])
+
+//.replace serve para trocar uma string por outra
+
+//console.log(frase.replace("Japão", "Brasil"));
+
+//.toLowerCase() serve para colocar as strings em minúsculas
+//.toUpperCase() serve para colocar as strings em maiúsculas
